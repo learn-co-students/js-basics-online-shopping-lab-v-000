7c9a43a385f586387764cad8a4d8719048c478f3
jjlippman <jjlippman@earthlink.net>
2016-04-17 14:06:56 -0600

    Done.

--- shopping.js
+++ shopping.js
@@ -1,1 +1,18 @@
 'use strict';
+
+var orderNumber;
+var total;
+var cardNumber;
+var cart=[];
+
+function addToCart (str) {
+  
+  var price = Math.floor((Math.random() * 100));;
+  
+  cart.push({item: price});
+  total+=price;
+  console.log( item + " has been added to your cart.");
+
+  console.log(cart.length);
+  return cart;
+}
