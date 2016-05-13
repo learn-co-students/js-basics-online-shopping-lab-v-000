576f785cd815b41494cbdf10051b1feb97023cf9
genericlady <yiannakokalas@gmail.com>
2016-05-13 11:19:23 -0400

    Done.

--- shopping.js
+++ shopping.js
@@ -1,11 +1,32 @@
-function total() {
-  let t = 0
+var cart = [];
 
-  for (let i = 0, l = cart.length; i < l; i++) {
-    for (let item in cart[i]) {
+function total() {
+  var t = 0
+
+  for (var i = 0, l = cart.length; i < l; i++) {
+    for (var item in cart[i]) {
       t += cart[i][item]
     }
   }
 
   return t
+}
+
+function getCart() {
+  return cart;
+}
+
+function setCart(contents) {
+  cart = contents;
+  return cart;
+}
+
+function addToCart(item) {
+  var price = (0..100);
+  cart.push(setItem(item, price));
+  return cart;
+}
+
+function setItem(item, price) {
+  return { [item] : [price] };
 }
