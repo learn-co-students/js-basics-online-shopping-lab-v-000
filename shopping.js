72e1faee7fdc36b6e4f40dce556126ccfcaab87f
Llubomir Stanojlovic <llustanojlovic@gmail.com>
2016-05-27 18:48:48 -0400

    Done.

--- shopping.js
+++ shopping.js
@@ -1,3 +1,14 @@
+var total=0;
+var cart = [];
+
+function getCart() {
+  return cart;
+}
+
+function setCart(array) {
+  cart=array;
+}
+
 function total() {
   let t = 0
 
@@ -6,6 +17,13 @@ function total() {
       t += cart[i][item]
     }
   }
-
   return t
 }
+
+function addToCart(item) {
+  var price = Math.floor(Math.random() * 100);
+  cart.push({item: price});
+  total += price;
+  console.log(item + " has been added to your cart.");
+  return cart;
+};
