652be3162620334bd7d66c7a27c0b9772fca216c
katherinethai <kthai1618@gmail.com>
2016-05-28 16:08:15 -0400

    Done.

--- shopping.js
+++ shopping.js
@@ -1,3 +1,5 @@
+var cart = []
+
 function total() {
   let t = 0
 
@@ -8,4 +10,35 @@ function total() {
   }
 
   return t
+}
+
+function getCart() {
+  return cart;
+}
+
+function setCart(array) {
+  cart = array;
+  return cart;
+}
+
+function addToCart(item) {
+  var price = Math.floor((Math.random() * 100));
+  item_object = {item: price};
+  cart.push(item_object);
+  console.log(`${item} has been added to your cart.`)
+  return cart;
+}
+
+function viewCart() {
+  if (cart.length) {
+    message = ("In your cart you have ");
+    body = [];
+    cart.forEach(function(item)) {
+      for (let itemObject in item) {
+        body.push(`${itemObject} at ${item[itemObject]}`);
+      }
+    }
+    body.join(', ');
+    console.log(message + body + ".");
+  }
 }
