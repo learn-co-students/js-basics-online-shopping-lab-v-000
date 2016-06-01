1d8414d779d7e8918bfe92e39b0af43a0674dbce
McN33ly <rmcneely23@gmail.com>
2016-06-01 01:34:49 -0400

    Done.

--- shopping.js
+++ shopping.js
@@ -1,4 +1,15 @@
 var cart = []
+function total() {
+  let t = 0
+
+  for (let i = 0, l = cart.length; i < l; i++) {
+    for (let item in cart[i]) {
+      t += cart[i][item]
+    }
+  }
+
+  return t
+}
 
 function getCart() {
   return cart
@@ -10,7 +21,8 @@ function setCart() {
 
 function addToCart(item) {
   var new_item = new Object();
-  cart.push(new_item[item] = Math.random())
+  new_item[item] = Math.floor(Math.random()*100)
+  cart.push(new_item)
   console.log(`${item} has been added to your cart.`)
   return cart
 }
@@ -20,17 +32,33 @@ function removeFromCart() {
 }
 
 function viewCart() {
-  console.log(cart)
-}
+  var cart_items = []
+  for(var i = 0; i < cart.length; i++){
+    for(let key in cart[i]) {
+      cart_items.push(`${key} at $${cart[i][key]}`)
 
-function total() {
-  let t = 0
-
-  for (let i = 0, l = cart.length; i < l; i++) {
-    for (let item in cart[i]) {
-      t += cart[i][item]
     }
   }
+  console.log(`In your cart, you have ` + cart_items.join(", ") + ".")
+}
 
-  return t
+function removeFromCart(item) {
+  function findItem(item){
+    return item === item
+  }
+  for(let item in cart){
+    delete cart[item]
+  }
+  if (!cart.find(findItem)) {
+    console.log("That item is not in your cart.")
+  }
+}
+
+function placeOrder(card) {
+  if (card == undefined) {
+    console.log("We don't have a credit card on file for you to place your order.")
+  } else {
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${card}.`)
+  }
+  cart.length = 0
 }
