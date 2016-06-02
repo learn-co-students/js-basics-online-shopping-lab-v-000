e2a37f77410ea9df891c2349e6a7e6d6dbdd4acb
jasonpaulso <jason@jasonpaulsouthwell.com>
2016-06-02 11:44:54 -0700

    Done.

--- shopping.js
+++ shopping.js
@@ -1,4 +1,7 @@
+var cart = []
+
 function total() {
+
   let t = 0
 
   for (let i = 0, l = cart.length; i < l; i++) {
@@ -9,3 +12,15 @@ function total() {
 
   return t
 }
+
+function getCart {
+  return cart
+}
+
+function setCart(array) {
+  var cart = array
+}
+
+// function addToCart(item) {
+//   Math.floor((Math.random() * 100) + 1);
+// }
