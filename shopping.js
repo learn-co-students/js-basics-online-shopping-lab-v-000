616bcab5489344515b10c9628dd62747a631e0d3
Andrew Papacostas <a_papacostas@yahoo.com>
2016-06-16 09:33:03 +0000

    Done.

--- shopping.js
+++ shopping.js
@@ -1,3 +1,39 @@
+var cart = [];
+
+function total() {
+  let t = 0
+
+  for (let i = 0, l = cart.length; i < l; i++) {
+    for (let item in cart[i]) {
+      t += cart[i][item]
+    }
+  }
+
+  return t
+}
+
+function getCart() {
+  return cart;
+}
+
+function setCart(arr) {
+  const cart = arr;
+}
+
+function addToCart(product) {
+  var price = Math.random((Math.floor() * 100);
+  cart.push({ [product]: price });
+  console.log(`${product}`);
+  return cart;
+}
+
+function viewCart() {
+  if (cart.length === 0) {
+
+  }
+
+}
+
 function total() {
   let t = 0
 
