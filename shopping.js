2fcfb5689fbac5ddc5df5a0472aa233cb44848c9
Nathaniel Miller <dukeoflaser@gmail.com>
2016-06-17 06:18:06 +0000

    Done.

--- shopping.js
+++ shopping.js
@@ -1,3 +1,5 @@
+var cart = []
+
 function total() {
   let t = 0
 
@@ -9,3 +11,38 @@ function total() {
 
   return t
 }
+
+function getCart(){
+  return cart;
+}
+
+function setCart(array){
+  cart = array;
+}
+
+function addToCart(item){
+  price = Math.floor(Math.random() * 100);
+  var forSale = {item: price};
+  cart.push(forSale);
+  console.log(`${item} has been added to your cart.`);
+  return cart;
+}
+
+function viewCart(){
+  if(cart.length == 0){
+    console.log("Your shopping cart is empty.");
+  } else {
+    var englishCart = [];
+    for(index in cart){
+      var forSale = cart[index]
+      for(item in forSale){
+        englishCart.push(`${item} at $${forSale[item]}`);
+        debugger;
+      }
+    }
+    console.log(`In your cart, you have ${englishCart.join(', ')}.`);
+  }
+}
+
+// cart = [{socks: 12}, {pizza: 40}]
+// viewCart(cart);
