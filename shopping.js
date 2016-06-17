3969403d161c3b4c81f51e38123b430cf0d333f8
Nathaniel Miller <dukeoflaser@gmail.com>
2016-06-17 21:25:47 +0000

    Done.

--- shopping.js
+++ shopping.js
@@ -31,18 +31,34 @@ function addToCart(item){
 function viewCart(){
   if(cart.length == 0){
     console.log("Your shopping cart is empty.");
+    return ("Your shopping cart is empty.");
   } else {
     var englishCart = [];
     for(index in cart){
       var forSale = cart[index]
       for(item in forSale){
         englishCart.push(`${item} at $${forSale[item]}`);
-        debugger;
       }
     }
     console.log(`In your cart, you have ${englishCart.join(', ')}.`);
+    return (`In your cart, you have ${englishCart.join(', ')}.`);
   }
 }
 
-// cart = [{socks: 12}, {pizza: 40}]
-// viewCart(cart);
+// cart = [{socks: 12}, {pizza: 40}];
+// viewCart();
+
+function removeFromCart(item){
+
+  for(index in cart){
+    if(cart[index].hasOwnProperty(item)){
+      cart.splice(index, 1);
+      return cart;
+    } else {
+      console.log("That item is not in your cart.");
+    }
+  }
+}
+
+// removeFromCart('pillow');
+// removeFromCart('socks');
