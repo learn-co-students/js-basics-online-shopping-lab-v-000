d270c8aa5a84747857849e7e3da801472a41a38e
adriandave13 <adrian@paradoxlogic.net>
2016-07-11 18:19:40 -0400

    Done.

--- shopping.js
+++ shopping.js
@@ -1,3 +1,5 @@
+var cart = [];
+
 function total() {
   let t = 0
 
@@ -8,4 +10,57 @@ function total() {
   }
 
   return t
+}
+
+function getCart() {
+  return cart;
+}
+
+function setCart(array) {
+  cart = array;
+}
+
+function addToCart(item) {
+  var price = Math.floor(Math.random() * 100)
+  cart.push({ [item]: price })
+  console.log(`${item} has been added to your cart.`)
+  return cart
+}
+
+function viewCart() {
+  if (!cart.length) {
+    console.log("Your shopping cart is empty.")
+  } else {
+    var itemsAndPrices = []
+    for (let i = 0; i < cart.length; i++) {
+      let itemAndPrice = cart[i]
+      let item = Object.keys(itemAndPrice)[0]
+      let price = itemAndPrice[item]
+      itemsAndPrices.push(`${item} at \$${price}`)
+      }
+    console.log(`In your cart, you have ${itemsAndPrices.join(', ')}.`)
+  }
+}
+
+function removeFromCart(item) {
+  let itemInCart = false
+
+  for (let i = 0; i < cart.length; i++) {
+    if (cart[i].hasOwnProperty(item)) {
+      itemInCart = true
+      cart = cart.slice(0, i).concat(cart.slice(i + 1))
+    }
+  }
+  if (!itemInCart) {
+    console.log("That item is not in your cart.")
+  }
+  return cart
+}
+
+function placeOrder(cardNumber) {
+  if (!cardNumber) {
+    return console.log("We don't have a credit card on file for you to place your order.")
+  }
+  console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
+  cart = []
 }
