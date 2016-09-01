c2f18e20a225a1a7190cf60be978fa25a19caa61
wedesignstudios <deelden@gmail.com>
2016-09-01 15:16:01 -0500

    Done

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var cart;
+var cart = [];
 
 function setCart(newCart) {
   cart = newCart;
@@ -14,4 +14,55 @@ function total() {
   }
 
   return t
+}
+
+function getCart() {
+  return cart;
+}
+
+function addToCart(item) {
+  let price = Math.floor(Math.random() * 100) + 1;
+  let itemPrice = {item: price};
+
+  cart.push(itemPrice);
+  console.log(`${item} has been added to your cart.`);
+  return cart;
+}
+
+function viewCart() {
+  if (cart.length === 0) {
+    return console.log("Your shopping cart is empty.");
+  }
+  
+  let itemAtPrice = [];
+
+  cart.forEach(function(things) {
+    for(var item in things){
+      itemAtPrice.push(item + " at $" + things[item]);
+    }
+  });    
+  console.log(`In your cart, you have ${itemAtPrice.join(", ")}.`);
+}
+
+function removeFromCart(itemName) {
+  itemInCart = false;
+
+  for (var i = 0; i < cart.length; i++) {
+    if (cart[i].hasOwnProperty(itemName)) {
+      itemInCart = true;
+      cart.splice([i], 1);
+    }
+  };
+  if (itemInCart === false) {
+    console.log("That item is not in your cart.");
+  }
+  return cart;  
+}
+
+function placeOrder(creditCardNumber) {
+  if (!creditCardNumber) {
+    return console.log("We don't have a credit card on file for you to place your order.");
+  }
+  console.log(`Your total cost is $${total()}, which will be charged to the card ${creditCardNumber}.`);
+  cart = [];
 }
