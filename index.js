a76e7f40f646bb5fd9643b51037b86c153b8c9de
rgylling <mikegylling@yahoo.com>
2016-10-09 10:38:00 -0700

    Done.

--- index.js
+++ index.js
@@ -15,3 +15,51 @@ function total() {
 
   return t
 }
+
+function getCart (){
+  return cart
+}
+
+function addToCart (item){
+  var randomNumber = Math.floor(Math.random() * (10));
+  var hash = {};
+  hash[item] = randomNumber;
+  cart.push(hash);
+  console.log(`${item} has been added to your cart.`)
+  return cart;
+}
+
+function viewCart(){
+  if (cart.length === 0) {
+    console.log("Your shopping cart is empty.");
+  } else {
+    var items = [];
+    for (let i = 0; i < cart.length; i++){
+      for (let key in cart[i]) {
+        items.push(`${key} at $${cart[i][key]}`);
+      }
+    }
+    console.log(`In your cart, you have ` + items.join(", ") + ".")
+  }
+}
+
+function removeFromCart(name){
+  for (var i = 0; i < cart.length; i++){
+    for (var key in cart[i]){
+      if (key === name) {
+        cart.splice(i, 1);
+        return cart;
+      }
+    }
+  }
+  console.log("That item is not in your cart.")
+}
+
+function placeOrder(number){
+  if (number === undefined) {
+    console.log("We don't have a credit card on file for you to place your order.")
+  } else {
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${number}.`)
+  }
+  return cart = [];
+}
