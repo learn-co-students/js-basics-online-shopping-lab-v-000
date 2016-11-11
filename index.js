666919d5a4b03ad9f6eb7f85b76cdba8a9446689
Lani1321 <lani.berry13@gmail.com>
2016-11-10 23:04:47 -0500

    add get cart and add to cart functions

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var cart;
+var cart = [];
 
 function setCart(newCart) {
   cart = newCart;
@@ -14,4 +14,42 @@ function total() {
   }
 
   return t
+}
+
+function getCart() {
+  return cart;
+}
+
+
+function addToCart(item) {
+  // Math.random gives you a random number, then math.floor rounds the number down
+  var price = Math.floor(Math.random() * 100);
+   cart.push({ [item]: price })
+
+  console.log(`${item} has been added to your cart.`)
+
+  return cart
+}
+
+
+function viewCart() {
+  
+  if (!cart.length) {
+    return console.log("Your shopping cart is empty.")
+  }
+
+  var itemPricesArray = []
+
+  for (let i = 0; i < cart.length; i++) {
+    let itemPriceArray = cart[i]
+    let item = Object.keys(itemPriceArray)[0]
+    let price = itemPriceArray[item]
+    itemPricesArray.push(`${item} at \$${price}`)
+  }
+    console.log(`In your cart, you have ${itemPricesArray.join(', ')}.`)
+
+}
+
+function removeFromCart(item) {
+
 }
