9abcf3623dc90cc4f2f8e42b75438bcd09311c90
bedle88 <robey.mc@gmail.com>
2016-11-21 14:14:47 -0500

    done

--- index.js
+++ index.js
@@ -1,4 +1,50 @@
-var cart;
+var cart = [];
+
+function getCart() {
+  return cart;
+}
+
+function addToCart(item) {
+  cart.push({[item]: Math.floor(Math.random() * 10)});
+  console.log(`${item} has been added to your cart.`)
+  return cart;
+}
+
+function viewCart() {
+  if (cart.length == 0) {
+    console.log ("Your shopping cart is empty.");
+  } else {
+    var itemPairs = [];
+    for (let i = 0; i < cart.length; i++) {
+      var item = Object.keys(cart[i])[0];
+      var price = cart[i][item];
+      itemPairs.push(`${item} at $${price}`);
+      }
+    console.log(`In your cart, you have ${itemPairs.join(', ')}.`);
+  }
+}
+
+function removeFromCart(item) {
+  let itemInCart = false
+  for (let i = 0, l = cart.length; i < l; i++) {
+    if (cart[i].hasOwnProperty(item)) {
+      cart.splice(i, 1);
+      return cart;
+    }
+  } if (!itemInCart) {
+    console.log("That item is not in your cart.");
+  }
+}
+
+function placeOrder(cardNumber) {
+  if (cardNumber == null) {
+    console.log("We don't have a credit card on file for you to place your order.");
+  } else {
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
+    setCart([])
+  }
+}
+
 
 function setCart(newCart) {
   cart = newCart;
