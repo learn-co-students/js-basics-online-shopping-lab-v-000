d829efca43a36fb442ff158308ebd1eae2f6055b
bmolina-nyc <bruce.molina.81@gmail.com>
2017-01-16 22:02:06 -0500

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-var cart;
+////was not able to run the tests in the console for whatever reason but the answers check out below using the babel compiler
+
+
+var cart = [] ;
+
+function getCart(){
+  return cart 
+}
 
 function setCart(newCart) {
   cart = newCart;
@@ -15,3 +22,52 @@ function total() {
 
   return t
 }
+
+function addToCart(item){
+  var itemPrice =  Math.floor((Math.random() * 100) + 1);
+  var newObject = {} 
+  newObject[item] = itemPrice
+  cart.push(newObject)
+  console.log(`${item} has been added to your cart.`)
+  return cart 
+}
+
+function viewCart(){
+  var answer = []
+  
+  if (cart.length === 0){
+    return "Your shopping cart is empty."
+  } else {
+    for (var i = 0; i < cart.length; i++){
+      answer.push(`${Object.keys(cart[i])} at $${Object.values(cart[i])}`)
+    }
+  }
+  answer[0] = "In your cart, you have" + ` ${answer[0]}`
+  var lastItem = `${answer.pop()}` + "."
+  answer.push(lastItem)
+  
+  return answer.join(", ")
+}  
+
+function removeFromCart(item){
+  for (var i = 0; i < cart.length; i++) {
+    if (cart[i].hasOwnProperty(item)) {
+      var itemFound = true 
+      delete cart[i]
+      setCart(cart.filter(Boolean));   //this guy here removes stuff thats falsey (like 'undefined')
+      break;
+    }
+  }
+  return "That item is not in your cart." 
+}
+
+
+
+function placeOrder(number){
+  if (number === undefined){
+    return "We don't have a credit card on file for you to place your order."
+  } else {
+    return `Your total cost is $${total()}, which will be charged to the card ${number}.`
+  }
+}
+
