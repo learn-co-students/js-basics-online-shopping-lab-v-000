023652f3c2b3eac7c74f3ad87eefcec62c3be40f
lizdenhup <lizdenhup@gmail.com>
2017-02-03 21:30:15 -0600

    6 tests passing, 5 to go

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var cart;
+var cart = new Array();
 
 function setCart(newCart) {
   cart = newCart;
@@ -6,12 +6,59 @@ function setCart(newCart) {
 
 function total() {
   let t = 0
-
   for (var i = 0, l = cart.length; i < l; i++) {
     for (var item in cart[i]) {
       t += cart[i][item]
     }
   }
-
   return t
 }
+
+function getCart() {
+  return cart;
+}
+
+function addToCart(item) {
+  const price = Math.floor(Math.random() * 100);
+  //the item and price are added as an object to the cart array. so cart
+  // is an array of objects
+  cart.push({[item]: price });
+  console.log(`${item} has been added to your cart.`);
+  return cart;
+}
+
+function viewCart() {
+  const stuffArray = [];
+    if (cart.length === 0) {
+      console.log("Your shopping cart is empty.")
+    } else {
+      for (var i = 0, l = cart.length; i < l; i++) {
+        //iterating through objects in the cart
+        let itemPricePair = cart[i];
+        //the item is the 0th element of each pair
+        let item = Object.keys(itemPricePair)[0];
+        //value = object[key]
+        let price = itemPricePair[item];
+        stuffArray.push(`${item} at \$${price}`)
+        console.log(`In your cart, you have ${stuffArray.join(', ')}.`)
+        }
+      }
+    }
+
+/*
+function removeFromCart(item) {
+  if (cart.includes(item)) {
+    var index = cart.indexOf(item);
+    if (index > -1) {
+        cart.splice(index, 1);
+    }
+  } else {
+    console.log("That item is not in your cart.")
+  }
+  return cart;
+}
+
+function placeOrder(creditCardNumber) {
+
+}
+*/
