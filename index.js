0a7986b000d689270c1373d23c0ba9f1fecd54d7
jennafrancis <jef399@nyu.edu>
2017-04-16 11:33:21 -0400

    Done.

--- index.js
+++ index.js
@@ -9,22 +9,51 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var price = Math.floor(Math.random() * 100)
+  cart.push({[item]: price})
+  console.log(`${item} has been added to your cart.`)
+  return cart
 }
 
 function viewCart() {
-  // write your code here
+  var array = [];
+   for (var i = 0; i < cart.length; i++) {
+     for (var item in cart[i]) {
+       array.push(`${item} at $${cart[i][item]}`);
+     }
+   }
+   if (cart.length < 1) {
+     console.log('Your shopping cart is empty.');
+   } else {
+     console.log(`In your cart, you have ${array.join(", ")}.`)
+   }
 }
 
 function total() {
-  // write your code here
+  var total = 0
+  for (var i = 0; i < cart.length; i++){
+    for (var item in cart[i]){
+      total += cart[i][item]
+    }
+  }
+  return total
 }
 
 function removeFromCart(item) {
-  // write your code here
+  for (let i = 0; i < cart.length; i++) {
+    if (cart[i].hasOwnProperty(item)){
+      cart.splice(i, 1)
+      return cart
+    }
+  }
+  console.log("That item is not in your cart.")
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (cardNumber === undefined) {
+    console.log("We don't have a credit card on file for you to place your order.")
+  } else {
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
+    cart.length = 0
+  }
 }
-
