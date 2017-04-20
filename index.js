b2016e566646a9978d20fa746d392046796eaea0
WilliamMena <williammena1991@gmail.com>
2017-04-19 21:27:32 -0400

    Done.

--- index.js
+++ index.js
@@ -10,10 +10,23 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+ cart[item] = Math.floor(Math.random() * 10)
+ // cart.push( {item:(Math.floor(Math.random())) } );
+ console.log(item + " has been added to your cart.")
+ return cart
 }
 
 function viewCart() {
   // write your code here
+  var cartArray = []
+  if (getCart().length > 0) {
+    for (var cartItem in cart) {
+      cartArray.push(`${cartItem} at ${cart[cartItem]}`)
+    }
+    console.log("In your cart, you have " + cartArray.join(', '))
+  } else {
+    console.log("Your shopping cart is empty.")
+  }
 }
 
 function total() {
