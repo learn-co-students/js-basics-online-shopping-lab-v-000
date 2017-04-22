3c5965c66632441d8574f93ce4db59a48d4de6a6
WilliamMena <williammena1991@gmail.com>
2017-04-22 17:31:02 -0400

    Done.

--- index.js
+++ index.js
@@ -90,5 +90,10 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   // write your code here
+  if () {
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
+  } else {
+    console.log("We don't have a credit card on file for you to place your order.")
+  }
 }
 
