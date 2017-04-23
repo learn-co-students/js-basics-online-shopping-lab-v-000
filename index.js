4040a31e970924425e91934975cac148fda2cc2c
Sholom Steinmetz <boruchsteinmetz@outlook.com>
2017-04-23 12:22:11 -0400

    Done.

--- index.js
+++ index.js
@@ -9,11 +9,25 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  getCart().push({[item]: Math.random()})
+  console.log(`${item} has been added to your cart.`)
+  return getCart()
 }
 
 function viewCart() {
-  // write your code here
+  var cart_list = new Array
+  for (var item = 0; item < getCart().length; item++) {
+    for (var name in getCart()[item]){
+
+    cart_list.push(` ${name} at $${getCart()[item][name]}`)
+    }
+  }
+  if (cart_list.length > 0) {
+    console.log(`In your cart, you have${cart_list.join(",")}.`)
+  }
+  else {
+    console.log("Your shopping cart is empty.")
+  }
 }
 
 function total() {
@@ -27,4 +41,3 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   // write your code here
 }
-
