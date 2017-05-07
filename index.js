6e8617d22adde88a951bcc83b9a7782da61a9c32
John Verdone <jmverdone@gmail.com>
2017-05-06 20:26:22 -0400

    Done.

--- index.js
+++ index.js
@@ -10,21 +10,58 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+ var price = Math.floor(Math.random()*100)
+
+ cart.push({[item]: price})
+  console.log(`${item} has been added to your cart.`);
+
+  return cart
 }
 
 function viewCart() {
   // write your code here
+  var cartItems = []
+  if(getCart().length > 0){
+    for (var i = 0; i < cart.length; i++) {
+      for( var item in cart[i]){
+        cartItems.push(` ${item} at $${cart[i][item]}`)
+      }
+    }
+    console.log(`In your cart, you have${cartItems}.`)
+  } else {
+    console.log("Your shopping cart is empty.")
+  }
 }
+
 
 function total() {
   // write your code here
+  let total = 0
+  for(let item in getCart()){
+   for( var price in cart[item])
+   total += cart[item][price]
+
+  }
+  return total
 }
 
 function removeFromCart(item) {
-  // write your code here
+  for(var name in cart){
+    if(cart[name].hasOwnProperty(item)){
+      cart.splice(name,1)
+      return cart
+    }
+  }
+  console.log("That item is not in your cart.")
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
-}
+  if(!cardNumber){
+    console.log("We don\'t have a credit card on file for you to place your order.")
+  } else {
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
 
+    setCart([])
+  }
+
+}
