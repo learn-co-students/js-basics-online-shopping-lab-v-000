a88216791958bd13a0682afcdd7ddf0d2068801c
github username <github email address>
2017-05-13 00:18:43 -0400

    Done.

--- index.js
+++ index.js
@@ -10,14 +10,35 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+ var price = Math.floor(Math.random() * 100) + 1
+ cart.push({[item]: price})
+ console.log(`${item} has been added to your cart.`)
+
+
+ return cart
 }
 
 function viewCart() {
   // write your code here
+  if (!cart.length) {
+  console.log(`Your shopping cart is empty.`)
 }
+
+  var array = []
+  for (var i = 0, l = cart.length; i < l; i++) {
+    var item = cart[i]
+    var price = cart[item]
+    var itemWithPrice = `${item[i]} at ${price}`
+    array.push(itemWithPrice)
+  }
+  console.log(`In your cart, you have ${array}.`)
+}
+
 
 function total() {
   // write your code here
+
+
 }
 
 function removeFromCart(item) {
@@ -27,4 +48,3 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   // write your code here
 }
-
