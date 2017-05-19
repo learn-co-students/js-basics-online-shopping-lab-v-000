e12f37189e93ee719e0539ad5ff222e3cfb254ae
wook kim <wooke2e@gmail.com>
2017-05-19 12:11:47 +0000

    Done.

--- index.js
+++ index.js
@@ -52,4 +52,10 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   // write your code here
+  if (cardNumber) {
+    console.log("Your total cost is $" + total() + ", which will be charged to the card " + cardNumber + ".")
+    cart = [];
+  } else {
+    console.log("We don't have a credit card on file for you to place your order.")
+  }
 }
