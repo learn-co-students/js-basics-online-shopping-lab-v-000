ee03f864848eff7f4fc7128a9cccba06610091f8
Daniel Suarez <suarez.daniel101@gmail.com>
2017-07-21 02:29:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,30 +1,46 @@
-var cart = []
+var cart = [];
 
 function getCart() {
  return cart
 }
 
 function setCart(c) {
-  cart = c
+  var c;
+  cart = c;
 }
 
 function addToCart(item) {
- // write your code here
+
+  var price = Math.floor(Math.random() * 100);
+  cart.push({[item]: price});
+  console.log(`${item} has been added to your cart.`);
+  return cart;
 }
 
 function viewCart() {
-  // write your code here
+  var text = "In your cart, you have ";
+  var text2 = "Your shopping cart is empty.";
+  if (cart.length === 0)
+  {
+    console.log(text2);
+  }
+  for(var i = 0; cart.length > i; i++)
+  {
+    if(cart.length -1 === i)
+    {
+      console.log(text += Object.keys(cart[i])[0] + " at $" + cart[i][Object.keys(cart[i])[0]] + ".");
+    } else
+    {
+      console.log(text += Object.keys(cart[i])[0] + " at $" + cart[i][Object.keys(cart[i])[0]] + ", ");
+    }
+  }
 }
 
 function total() {
-  // write your code here
+  var totalCost = 0;
+  for(var i = 0; cart.length > i; i++)
+  {
+    totalCost += cart[i][Object.keys(cart[i])[0]];
+  }
+  return totalCost;
 }
-
-function removeFromCart(item) {
-  // write your code here
-}
-
-function placeOrder(cardNumber) {
-  // write your code here
-}
-
