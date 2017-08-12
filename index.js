9abbe92d55cfc7c590798ba0eeef99c3ada52683
Bruce Molina <bruce.molina.81@gmail.com>
2017-08-12 16:08:33 +0000

    Done.

--- index.js
+++ index.js
@@ -1,78 +1,83 @@
-////was not able to run the tests in the console for whatever reason but the answers check out below using the babel compiler
+var cart = [];
 
-
-var cart = [] ;
-
-function getCart(){
-  return cart 
+function getCart() {
+ return cart;
 }
 
-function setCart(newCart) {
-  cart = newCart;
+function setCart(c) {
+  cart = c;
+  return cart;
+}
+
+function addToCart(item) {
+ // write your code here
+ var meals = { [item]: Math.floor(Math.random() * 100) }
+ cart.push(meals)
+ console.log(`${item} has been added to your cart.`)
+ return cart
+}
+
+
+
+function viewCart() {
+  // write your code here
+  if (cart.length === 0) {
+    return console.log("Your shopping cart is empty.")
+  }
+
+  var sentence = "In your cart, you have "
+
+  if (cart.length > 1){
+    var last_item = ` and ${Object.keys(cart[cart.length -1 ]).join()} at $${Object.values(cart[cart.length -1 ]).join()}`
+
+    for (let i = 0 ; i < cart.length -1 ; i++){
+      sentence += `${Object.keys(cart[i]).join()} at $${Object.values(cart[i]).join()}, `
+    }
+  } else if (cart.length === 1){
+    return console.log(sentence + `${Object.keys(cart[0]).join()} at $${Object.values(cart[0]).join()}.`)
+  }
+
+  if (cart.length === 2){
+   return console.log(`In your cart, you have ${Object.keys(cart[0]).join()} at $${Object.values(cart[0]).join()}${last_item}.`)
+} else {
+  var final_item = `and ${Object.keys(cart[cart.length -1 ]).join()} at $${Object.values(cart[cart.length -1 ]).join()}`
+  return console.log( sentence + final_item + ".")
+}
+
 }
 
 function total() {
-  let t = 0
+  // write your code here
+var total = 0
 
-  for (var i = 0, l = cart.length; i < l; i++) {
-    for (var item in cart[i]) {
-      t += cart[i][item]
+  for (let i=0; i< cart.length; i++){
+    total += parseInt(Object.values(cart[i]).join())
+  }
+
+  return `$${total}`
+}
+
+function removeFromCart(item) {
+  // write your code here
+
+  for (let i = 0 ; i < cart.length ; i ++){
+    if (cart[i].hasOwnProperty(item)){
+      cart.splice(cart[i], 1)
+      console.log(`${item} has been removed from your cart`)
+      return getCart()
     }
   }
 
-  return t
+  console.log("That item is not in your cart.")
+  getCart()
 }
 
-function addToCart(item){
-  var itemPrice =  Math.floor((Math.random() * 100) + 1);
-  var newObject = {} 
-  newObject[item] = itemPrice
-  cart.push(newObject)
-  console.log(`${item} has been added to your cart.`)
-  return cart 
-}
-
-function viewCart(){
-  var answer = []
-  
-  if (cart.length === 0){
-    return "Your shopping cart is empty."
+function placeOrder(cardNumber) {
+  // write your code here
+  if (cardNumber === undefined) {
+    return console.log( "Sorry, we don't have a credit card on file for you.")
   } else {
-    for (var i = 0; i < cart.length; i++){
-      answer.push(`${Object.keys(cart[i])} at $${Object.values(cart[i])}`)
-    }
+    console.log(`Your total cost is ${total()}, which will be charged to the card ${cardNumber}.`)
+    cart = []
   }
-  answer[0] = "In your cart, you have" + ` ${answer[0]}`
-  var lastItem = `${answer.pop()}` + "."
-  answer.push(lastItem)
-  
-  return answer.join(", ")
-}  
-
-function removeFromCart(item){
-  for (var i = 0; i < cart.length; i++) {
-    if (cart[i].hasOwnProperty(item)) {
-      var itemFound = true 
-      delete cart[i]
-      setCart(cart.filter(Boolean));   //this guy here removes stuff thats falsey (like 'undefined')
-      break;
-    }
-  }
-  if (!itemFound){
-    return "That item is not in your cart." 
-  }
-  return cart 
 }
-
-
-function placeOrder(number){
-  if (number === undefined){
-    return "We don't have a credit card on file for you to place your order."
-  } else {
-    console.log( `Your total cost is $${total()}, which will be charged to the card ${number}.`)
-  }
-  cart = []
-  return cart
-}
-
-
