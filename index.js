c43aba6ad5c28e667f5eb1fcf71ec9484534d86e
github username pianistps <pianistps@yahoo.com>
2017-09-19 21:24:38 -0500

    Done.

--- index.js
+++ index.js
@@ -9,22 +9,78 @@ function setCart(c) {
   return cart;
 }
 
+function getRandomIntInclusive() {
+  var min = Math.ceil(1);
+  var max = Math.floor(100);
+  return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive
+}
+
 function addToCart(item) {
- // write your code here
+ var newItem = Object.assign({}, {[item]: getRandomIntInclusive()})
+  cart.push(newItem)
+  console.log(`${item} has been added to your cart.`)
+  return cart
 }
 
 function viewCart() {
-  // write your code here
+  if (cart.length < 1){
+    console.log("Your shopping cart is empty.")
+  }
+  var string = `In your cart, you have `
+  var lastIteration = cart.length - 1
+  for (var i = 0; i < cart.length; i++){
+    var item = cart[i];
+    var key = Object.keys(item);
+    var price = item[key]
+    if(cart.length === 1) {
+      string += `${key} at $${price}.`
+    } else if (cart.length === 2) {
+      if (i === 0) {
+        string += `${key} at $${price} `
+      } else {
+        string += `and ${key} at $${price}.`
+      }
+    } else {
+      if (i === 0) {
+        string += `${key} at $${price}`
+      } else if (i === lastIteration) {
+        string += `, and ${key} at $${price}.`
+      } else {
+        string += `, ${key} at $${price}`
+      }
+    }
+  }
+  console.log(string)
 }
 
 function total() {
-  // write your code here
+  var total = 0
+  for(var i = 0; i < cart.length; i++){
+    for (let item in cart[i]) {
+      total += cart[i][item];
+    }
+  }
+  return total
 }
 
 function removeFromCart(item) {
-  // write your code here
+  for (var i = 0; i < cart.length; i++) {
+    var item1 = cart[i];
+    var key = Object.keys(item1);
+    debugger
+    if (key[0] === item) {
+        cart.splice(i , 1)
+      }
+  }
+  console.log("That item is not in your cart.")
+  return cart
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (!cardNumber) {
+    console.log("Sorry, we don't have a credit card on file for you.")
+  } else {
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
+    cart.splice(0, cart.length)
+  }
 }
